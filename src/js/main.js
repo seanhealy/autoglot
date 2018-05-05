99cd2396f7ac440b9f3fa2242514ef795e88890a
Dan <djphan@gmail.com>
2018-05-05 15:07:22 -0600

    User input for data

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,10 @@
-const sampleFloatingPointNumber = 0.8;
+let sampleFloatingPointNumber = (Math.random() * (0.15 - 0.1) + 0.1).toFixed(4);
 
 const talkName = document.getElementById("talk-name");
 const talkType = document.getElementById("talk-type");
 const button = document.getElementById("button");
+const floatTextField = document.getElementById("floating-point-input");
+const floatButton = document.getElementById("floating-point-button");
 
 const talkTypeArray = [
   'Fish Bowl Discussion',
@@ -16,10 +18,21 @@ function randomizer(inputNumber) {
   return Math.floor(Math.random() * inputNumber);
 }
 
+
 talkName.textContent = sampleNetwork(sampleFloatingPointNumber).sentence
 talkType.textContent = talkTypeArray[randomizer(talkTypeArray.length)];
+floatTextField.value = sampleFloatingPointNumber;
 
 button.onclick = () => {
   talkName.textContent = sampleNetwork(sampleFloatingPointNumber).sentence;
   talkType.textContent = talkTypeArray[randomizer(talkTypeArray.length)];
 };
+
+floatButton.onclick = () => {
+  if (floatTextField.value  > 0.15 || floatTextField.value  < 0 || isNaN(floatTextField.value)) {
+    alert('Input Value = must be between 0 and 0.15. Did not update floating point number.');
+  } else {
+    alert('Success');
+    sampleFloatingPointNumber = floatTextField.value; 
+  }
+}

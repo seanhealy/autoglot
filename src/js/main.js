33f142d62c89832d0b4879a3a7222026d8f9e4ce
Sean Healy <s@xib.ca>
2018-05-05 15:15:48 -0600

    quick fix for what I broke in a really dumb way

--- src/js/main.js
+++ src/js/main.js
@@ -3,23 +3,22 @@ let sampleFloatingPointNumber = (Math.random() * (0.15 - 0.1) + 0.1).toFixed(4);
 const talkName = document.getElementById("talk-name");
 const talkType = document.getElementById("talk-type");
 const button = document.getElementById("button");
-const floatTextField = document.getElementById("floating-point-input");
+const floatTextField = document.getElementById("floating-point-input") || 0;
 const floatButton = document.getElementById("floating-point-button");
 
 const talkTypeArray = [
-  'Fish Bowl Discussion',
-  'Prepared Talk',
-  'Mob Programming/Hackathon',
-  'Round Table',
-  'World Cafe',
+  "Fish Bowl Discussion",
+  "Prepared Talk",
+  "Mob Programming/Hackathon",
+  "Round Table",
+  "World Cafe"
 ];
 
 function randomizer(inputNumber) {
   return Math.floor(Math.random() * inputNumber);
 }
 
-
-talkName.textContent = sampleNetwork(sampleFloatingPointNumber).sentence
+talkName.textContent = sampleNetwork(sampleFloatingPointNumber).sentence;
 talkType.textContent = talkTypeArray[randomizer(talkTypeArray.length)];
 floatTextField.value = sampleFloatingPointNumber;
 
@@ -28,11 +27,19 @@ button.onclick = () => {
   talkType.textContent = talkTypeArray[randomizer(talkTypeArray.length)];
 };
 
-floatButton.onclick = () => {
-  if (floatTextField.value  > 0.15 || floatTextField.value  < 0 || isNaN(floatTextField.value)) {
-    alert('Input Value = must be between 0 and 0.15. Did not update floating point number.');
-  } else {
-    alert('Success');
-    sampleFloatingPointNumber = floatTextField.value; 
-  }
+if (floatButton) {
+  floatButton.onclick = () => {
+    if (
+      floatTextField.value > 0.15 ||
+      floatTextField.value < 0 ||
+      isNaN(floatTextField.value)
+    ) {
+      alert(
+        "Input Value = must be between 0 and 0.15. Did not update floating point number."
+      );
+    } else {
+      alert("Success");
+      sampleFloatingPointNumber = floatTextField.value;
+    }
+  };
 }
